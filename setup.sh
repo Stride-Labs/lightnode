c4380fcdef0e2aac8876e650c861ff17dba2ed9a
Riley Edmunds <riley@stridelabs.co>
2024-07-11 23:32:25 +0200

    pruning added

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 # download run script to re-run later
-echo downloading script
+echo downloading execution script...
 URL="https://raw.githubusercontent.com/Stride-Labs/lightnode/main/zzz.sh"
 curl -sL $URL > zzz.sh
 
+echo downloading pruning script...
+URL="https://raw.githubusercontent.com/Stride-Labs/lightnode/main/prune.sh"
+curl -sL $URL > prune.sh
+
 # setup
-echo setting up node
+echo setting up node...
 sudo apk update
 sudo apk add curl tar wget pkgconfig libressl-dev jq git bash make
 sudo apk add go
@@ -14,7 +18,5 @@ git checkout v0.14.0
 make build
 build/celestia light init
 
-echo pruning node
-./celestia-node/build/celestia light unsafe-reset-store --p2p.network celestia
-echo starting up node
-./celestia-node/build/celestia light start --core.ip rpc.celestia.pops.one --p2p.network celestia --experimental-pruning
+echo starting up node...
+build/celestia light start --core.ip rpc.celestia.pops.one --p2p.network celestia --experimental-pruning
--- zzz.sh
+++ zzz.sh
@@ -1,4 +1,2 @@
-echo pruning node
-./celestia-node/build/celestia light unsafe-reset-store --p2p.network celestia
-echo running node
+echo running node...
 ./celestia-node/build/celestia light start --core.ip rpc.celestia.pops.one --p2p.network celestia --experimental-pruning
