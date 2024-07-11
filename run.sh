618decd536a9cd000f6506d444d944f4d55abae9
Riley Edmunds <riley@stridelabs.co>
2024-07-11 23:16:36 +0200

    setup

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo pruning node
-celestia light unsafe-reset-store --p2p.network celestia
+./celestia-node/build/celestia light unsafe-reset-store --p2p.network celestia
 echo running node
-build/celestia light start --core.ip rpc.celestia.pops.one --p2p.network celestia --experimental-pruning
+./celestia-node/build/celestia light start --core.ip rpc.celestia.pops.one --p2p.network celestia --experimental-pruning
